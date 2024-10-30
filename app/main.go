6d1197c27425515238af19c8bb5dd357621e2048
peedrou <pedrofsalesvieira@gmail.com>
2024-10-30 17:37:33 +0100

    testing first database operations

--- app/main.go
+++ app/main.go
@@ -1,9 +1,53 @@
 package main
 
 import (
+	"VieiraDJS/app/services/jobs"
 	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
+
+	"strings"
+
+	"github.com/gocql/gocql"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	cassandraHosts := os.Getenv("CASSANDRA_HOSTS")
+	cassandraPort := os.Getenv("CASSANDRA_PORT")
+	cassandraKeyspace := os.Getenv("CASSANDRA_KEYSPACE")
+
+	cluster := gocql.NewCluster(strings.Split(cassandraHosts, ",")...)
+	cluster.Port = parsePort(cassandraPort)
+	cluster.Keyspace = cassandraKeyspace
+	cluster.Consistency = gocql.Quorum
+
+	session, err := cluster.CreateSession()
+	if err != nil {
+		log.Fatalf("Failed to connect to Cassandra: %v", err)
+	}
+	defer session.Close()
+
+	err = jobs.CreateJob(session, true, 3, time.Now(), "1h")
+	if err != nil {
+		fmt.Printf("Error creating job: %v\n", err)
+		return
+	}
+
+	fmt.Println("Job successfully created and inserted into Cassandra!")
+}
+
+func parsePort(port string) int {
+	portInt, err := strconv.Atoi(port)
+	if err != nil {
+		log.Fatalf("Invalid port: %v", err)
+	}
+	return portInt
 }
