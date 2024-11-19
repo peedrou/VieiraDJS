218a331d2abf71cb5c415b0a65f56dc3682ad1fc
peedrou <pedrofsalesvieira@gmail.com>
2024-11-19 17:32:41 +0100

    first try at sending a message to a topic

--- app/main.go
+++ app/main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
-	crud "VieiraDJS/app/db/CRUD"
-	"VieiraDJS/app/services/jobs"
-	"VieiraDJS/app/services/users"
-	"fmt"
+	// crud "VieiraDJS/app/db/CRUD"
+	// "VieiraDJS/app/services/jobs"
+	// "VieiraDJS/app/services/users"
+	// "fmt"
+	"VieiraDJS/app/kafka"
 	"log"
 	"os"
 	"strconv"
-	"time"
+
+	// "time"
 
 	"strings"
 
@@ -25,47 +27,54 @@ func main() {
 	cassandraHosts := os.Getenv("CASSANDRA_HOSTS")
 	cassandraPort := os.Getenv("CASSANDRA_PORT")
 	cassandraKeyspace := os.Getenv("CASSANDRA_KEYSPACE")
+	kafkaBrokers := os.Getenv("KAFKA_BROKERS")
 
 	cluster := gocql.NewCluster(strings.Split(cassandraHosts, ",")...)
 	cluster.Port = parsePort(cassandraPort)
 	cluster.Keyspace = cassandraKeyspace
 	cluster.Consistency = gocql.Quorum
 
-	session, err := cluster.CreateSession()
-	if err != nil {
-		log.Fatalf("Failed to connect to Cassandra: %v", err)
-	}
-	defer session.Close()
+	producer, err := kafka.NewKafkaProducer([]string{kafkaBrokers})
 
-	user_id, err := users.RegisterUser(session, "testuser6", "hellopassword", "thisisanemail6@gmail.com")
-	if err != nil {
-		fmt.Printf("Error creating user: %v\n", err)
-		return
-	}
+	producer.SendMessage("my_new_topic", "Hello Im a message!")
 
-	err = jobs.CreateJob(session, user_id, true, 3, time.Now(), "8H")
-	if err != nil {
-		fmt.Printf("Error creating job: %v\n", err)
-		return
-	}
+	producer.Close()
 
-	fmt.Println("Job successfully created and inserted into Cassandra!")
+	// session, err := cluster.CreateSession()
+	// if err != nil {
+	// 	log.Fatalf("Failed to connect to Cassandra: %v", err)
+	// }
+	// defer session.Close()
 
-	result, _ := crud.ReadModel(session, "jobs", []string{"job_id"}, []string{"interval"}, "2h")
+	// user_id, err := users.RegisterUser(session, "testuser6", "hellopassword", "thisisanemail6@gmail.com")
+	// if err != nil {
+	// 	fmt.Printf("Error creating user: %v\n", err)
+	// 	return
+	// }
 
-	fmt.Printf("job successfully read from Cassandra! %v", result)
+	// err = jobs.CreateJob(session, user_id, true, 3, time.Now(), "8H")
+	// if err != nil {
+	// 	fmt.Printf("Error creating job: %v\n", err)
+	// 	return
+	// }
 
-	err = crud.UpdateModelBatch(session, "jobs", "interval", "6H", "job_id", result)
-	if err != nil {
-		fmt.Printf("Error Deleting model: %v\n", err)
-		return
-	}
+	// fmt.Println("Job successfully created and inserted into Cassandra!")
 
-	err = crud.RemoveModel(session, "jobs", "job_id", result)
-	if err != nil {
-		fmt.Printf("Error Deleting model: %v\n", err)
-		return
-	}
+	// result, _ := crud.ReadModel(session, "jobs", []string{"job_id"}, []string{"interval"}, "2h")
+
+	// fmt.Printf("job successfully read from Cassandra! %v", result)
+
+	// err = crud.UpdateModelBatch(session, "jobs", "interval", "6H", "job_id", result)
+	// if err != nil {
+	// 	fmt.Printf("Error Deleting model: %v\n", err)
+	// 	return
+	// }
+
+	// err = crud.RemoveModel(session, "jobs", "job_id", result)
+	// if err != nil {
+	// 	fmt.Printf("Error Deleting model: %v\n", err)
+	// 	return
+	// }
 }
 
 func parsePort(port string) int {
