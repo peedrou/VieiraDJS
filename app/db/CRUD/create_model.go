e2adb907462e4af3e36e2a9953e55422811be809
peedrou <pedrofsalesvieira@gmail.com>
2024-11-25 16:48:54 +0100

    fixing kafka sendmessage error

--- app/db/CRUD/create_model.go
+++ app/db/CRUD/create_model.go
@@ -30,3 +30,32 @@ func CreateModel(session *gocql.Session, tableName string, fieldNames []string, 
 	return nil
 
 }
+
+func CreateModelBatch(session *gocql.Session, tableName string, fieldNames []string, batchValues [][]interface{}) error {
+
+	batch := session.NewBatch(gocql.LoggedBatch)
+
+	for _, values := range batchValues {
+		var placeholders []string
+		len := len(values)
+
+		for i := 0; i < len; i++ {
+			placeholders = append(placeholders, "?")
+		}
+
+		query := fmt.Sprintf(
+			"INSERT INTO %s (%s) VALUES (%s)",
+			tableName,
+			strings.Join(fieldNames, ", "),
+			strings.Join(placeholders, ", "),
+		)
+
+		batch.Query(query, values...)
+	}
+
+	if err := session.ExecuteBatch(batch); err != nil {
+		return fmt.Errorf("failed to execute batch insert into %s: %v", tableName, err)
+	}
+
+	return nil
+}
--- app/services/scheduler/scheduling_service.go
+++ app/services/scheduler/scheduling_service.go
@@ -3,7 +3,9 @@ package scheduler
 import (
 	crud "VieiraDJS/app/db/CRUD"
 	"VieiraDJS/app/helpers/converters"
+	"VieiraDJS/app/kafka"
 	"fmt"
+	"log"
 	"time"
 
 	"github.com/gocql/gocql"
@@ -19,10 +21,35 @@ func CheckPendingTasks(session *gocql.Session) ([]interface{}, error) {
 		[]string{"job_id"},
 		[]string{"next_execution_time"},
 		nextMinuteExecutionTime)
-	
+
 	if err != nil {
 		return nil, fmt.Errorf("failed to fetch this minute's tasks: %v", err)
 	}
 
 	return result, nil
+}
+
+func SchedulePendingTasks(kp *kafka.KafkaProducer, tasks []interface{}) ([]interface{}, []interface{}, error) {
+	var tasksSucceeded []interface{}
+	var tasksFailed []interface{}
+
+	for _, task := range tasks {
+		taskMessage, ok := task.(string)
+		if !ok {
+			return tasksSucceeded, tasksFailed, fmt.Errorf("invalid task format: %v", task)
+		}
+
+		topic := "task-schedule"
+
+		err := kp.SendMessage(topic, taskMessage)
+		if err != nil {
+			log.Printf("failed to send task message: %v", err)
+			tasksFailed = append(tasksFailed, task)
+		} else {
+			log.Printf("Task sent: %s", taskMessage)
+			tasksSucceeded = append(tasksSucceeded, task)
+		}
+	}
+
+	return tasksSucceeded, tasksFailed, nil
 }
